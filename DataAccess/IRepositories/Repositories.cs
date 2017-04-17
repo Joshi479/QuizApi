213147aee37a376b69d82540b4b9d2b7573db07c
Joshi479 <joshisaiteja79@gmail.com>
2017-04-17 08:23:53 -0700

    Add app

---
+++ DataAccess/IRepositories/Repositories.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public interface ICategoryRepository : IQuizRepository<Categories>
+    {
+
+    }
+    public interface IQuestionLeveRepository : IQuizRepository<QuestionLevels>
+    {
+
+    }
+    public interface IQuestionRepository : IQuizRepository<Questions>
+    {
+
+    }
+    public interface IAnswerChoiceRepository : IQuizRepository<AnswerChoices>
+    {
+
+    }
+    public interface ISubCategoryRepository : IQuizRepository<SubCategories>
+    {
+
+    }
+}
+
